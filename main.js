c3ce8bed137606d582d831132b7721932ce35608
HussnainProg <husnain6908087@gmail.com>
2024-08-06 15:51:13 +0500

    Implementing the functionality  of converting image into text

--- main.js
+++ main.js
@@ -2,8 +2,20 @@ const btn = document.querySelector("#submit");
 const text = document.getElementsByTagName("textarea");
 const selectOption = document.querySelector("#optionsSelect");
 let voices = [];
+let btn1 = document.getElementById("option-1");
+let btn2 = document.getElementById("option-2");
+// console.log(btn1);
+// console.log(btn2);
+btn1.addEventListener('click',(e)=>{
+  document.querySelector("#text-to-speech").style.display = "block";
+  document.querySelector("#img-to-text").style.display  = "none";
+  
+})
+btn2.addEventListener('click',(e)=>{
+  document.querySelector("#img-to-text").style.display  = "block";
+  document.querySelector("#text-to-speech").style.display = "none"; 
 
-// console.log(btn,text);
+})
 
 // This metho is creating an option and populating it in select options
 function addVoiceList() {
@@ -40,3 +52,38 @@ btn.addEventListener("click", (event) => {
     event.preventDefault();
   }
 });
+
+// Implementing the second option of side menu
+let selectFile;
+document.getElementById("selectFile").addEventListener('change',(e)=>{
+  selectFile = e.target.files[0]; 
+
+}) 
+function generateText(){
+  let btn = document.getElementById("imageInput");
+  console.log(btn);
+  btn.addEventListener('click', function (event) {
+    // const file = event.target.files[0];
+    if (selectFile) {
+      const reader = new FileReader();
+      reader.onload = function () {
+          Tesseract.recognize(
+              reader.result,
+              'eng',
+              {
+                  logger: m => console.log(m)
+              }
+            ).then(({ data: { text } }) => {
+              document.getElementById('output').innerText = text;
+          }).catch(err => {
+              console.error(err);
+          });
+      };
+      reader.readAsDataURL(selectFile);
+  }
+});
+// selectFile;
+}
+
+generateText();
+
